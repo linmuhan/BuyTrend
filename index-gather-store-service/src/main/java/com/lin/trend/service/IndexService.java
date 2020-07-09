d38888606a19b8acca728fb681211f2a5c78ad99
CryFace <linshanting8068@qq.com>
2020-07-09 16:19:10 +0800

    集成Redis来对转储的数据进行持久化

--- index-gather-store-service/src/main/java/com/lin/trend/service/IndexService.java
+++ index-gather-store-service/src/main/java/com/lin/trend/service/IndexService.java
@@ -1,7 +1,11 @@
 package com.lin.trend.service;
 
+import cn.hutool.core.collection.CollectionUtil;
 import com.lin.trend.pojo.Index;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
  
@@ -10,10 +14,13 @@ import java.util.List;
 import java.util.Map;
  
 @Service
+@CacheConfig(cacheNames="indexes")
 public class IndexService {
     private List<Index> indexes;
     @Autowired RestTemplate restTemplate;
- 
+
+    @HystrixCommand(fallbackMethod = "third_part_not_connected")
+    @Cacheable(key="'all_codes'")
     public List<Index> fetch_indexes_from_third_part(){
         List<Map> temp= restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json",List.class);
         return map2Index(temp);
@@ -33,6 +40,14 @@ public class IndexService {
         }
  
         return indexes;
+    }
+
+    public List<Index> third_part_not_connected(){
+        System.out.println("third_part_not_connected()");
+        Index index= new Index();
+        index.setCode("000000");
+        index.setName("无效指数代码");
+        return CollectionUtil.toList(index);
     }
  
 }
