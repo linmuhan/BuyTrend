870e82bde3efb2cf56e575a464c57038977704cc
CryFace <linshanting8068@qq.com>
2020-07-09 11:21:55 +0800

    First Commit

---
+++ eureka-server/src/test/java/com/lin/trend/AppTest.java
@@ -0,0 +1,20 @@
+package com.lin.trend;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
