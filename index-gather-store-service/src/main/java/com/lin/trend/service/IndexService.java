f952507f1c9e87bfd0885786d3c22441a529c242
CryFace <linshanting8068@qq.com>
2020-07-09 17:08:28 +0800

    完善了Redis的数据的因为熔断后后续无法刷新的问题

--- index-gather-store-service/src/main/java/com/lin/trend/service/IndexService.java
+++ index-gather-store-service/src/main/java/com/lin/trend/service/IndexService.java
@@ -1,18 +1,21 @@
 package com.lin.trend.service;
 
-import cn.hutool.core.collection.CollectionUtil;
 import com.lin.trend.pojo.Index;
+import com.lin.trend.util.SpringContextUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.collection.CollectionUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
- 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
- 
+
 @Service
 @CacheConfig(cacheNames="indexes")
 public class IndexService {
@@ -20,14 +23,36 @@ public class IndexService {
     @Autowired RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "third_part_not_connected")
+    public List<Index> fresh() {
+        indexes =fetch_indexes_from_third_part();
+        IndexService indexService = SpringContextUtil.getBean(IndexService.class);
+        indexService.remove();
+        return indexService.store();
+    }
+
+    //删除数据
+    @CacheEvict(allEntries=true)
+    public void remove(){
+
+    }
+
+    //保存数据
     @Cacheable(key="'all_codes'")
+    public List<Index> store(){
+        System.out.println(this);
+        return indexes;
+    }
+
+    //获取数据
+    @Cacheable(key="'all_codes'")
+    public List<Index> get(){
+        return CollUtil.toList();
+    }
+
     public List<Index> fetch_indexes_from_third_part(){
         List<Map> temp= restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json",List.class);
         return map2Index(temp);
     }
-
-
-    //通过http通信获得到的数据是map类型的，所以我们要将数据转换成我们需要的list类型即可
     private List<Index> map2Index(List<Map> temp) {
         List<Index> indexes = new ArrayList<>();
         for (Map map : temp) {
@@ -38,7 +63,7 @@ public class IndexService {
             index.setName(name);
             indexes.add(index);
         }
- 
+
         return indexes;
     }
 
@@ -49,5 +74,5 @@ public class IndexService {
         index.setName("无效指数代码");
         return CollectionUtil.toList(index);
     }
- 
+
 }
--- index-gather-store-service/src/main/java/com/lin/trend/web/IndexController.java
+++ index-gather-store-service/src/main/java/com/lin/trend/web/IndexController.java
@@ -12,9 +12,18 @@ import com.lin.trend.service.IndexService;
 @RestController
 public class IndexController {
     @Autowired IndexService indexService;
- 
+
+    @GetMapping("/freshCodes")
+    public List<Index> fresh() throws Exception {
+        return indexService.fresh();
+    }
     @GetMapping("/getCodes")
     public List<Index> get() throws Exception {
-        return indexService.fetch_indexes_from_third_part();
+        return indexService.get();
+    }
+    @GetMapping("/removeCodes")
+    public String remove() throws Exception {
+        indexService.remove();
+        return "remove codes successfully";
     }
 }
