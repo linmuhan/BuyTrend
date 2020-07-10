239e72d11d9bcc49d62caf3f74a1eb525e0e2472
CryFace <linshanting8068@qq.com>
2020-07-10 20:01:03 +0800

    配置了对多个数据提供服务的网关

---
+++ index-zuul-service/src/main/java/com/lin/trend/IndexZuulServiceApplication.java
@@ -0,0 +1,26 @@
+package com.lin.trend;
+
+import cn.hutool.core.util.NetUtil;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class IndexZuulServiceApplication {
+    //  http://127.0.0.1:8031/api-codes/codes
+    public static void main(String[] args) {
+        int port = 8032;
+        if(!NetUtil.isUsableLocalPort(port)) {
+            System.err.printf("端口%d被占用了，无法启动%n", port );
+            System.exit(1);
+        }
+        new SpringApplicationBuilder(IndexZuulServiceApplication.class).properties("server.port=" + port).run(args);
+
+    }
+
+}
