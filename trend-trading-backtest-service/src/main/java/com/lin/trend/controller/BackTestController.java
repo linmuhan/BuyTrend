308932ab132eee53f948e369a7b05f267daff5e5
CryFace <linshanting8068@qq.com>
2020-07-11 11:03:45 +0800

    增加了对指数回测数据的可控日期范围图表显示

--- trend-trading-backtest-service/src/main/java/com/lin/trend/controller/BackTestController.java
+++ trend-trading-backtest-service/src/main/java/com/lin/trend/controller/BackTestController.java
@@ -2,26 +2,59 @@ package com.lin.trend.controller;
 
 import com.lin.trend.pojo.IndexData;
 import com.lin.trend.service.BackTestService;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
- 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-  
+
+import java.util.*;
+
 @RestController
 public class BackTestController {
     @Autowired BackTestService backTestService;
- 
-    @GetMapping("/simulate/{code}")
+
+    @GetMapping("/simulate/{code}/{startDate}/{endDate}")
     @CrossOrigin
-    public Map<String,Object> backTest(@PathVariable("code") String code) throws Exception {
+    public Map<String,Object> backTest(
+            @PathVariable("code") String code
+            ,@PathVariable("startDate") String strStartDate
+            ,@PathVariable("endDate") String strEndDate
+    ) throws Exception {
         List<IndexData> allIndexDatas = backTestService.listIndexData(code);
+
+        String indexStartDate = allIndexDatas.get(0).getDate();
+        String indexEndDate = allIndexDatas.get(allIndexDatas.size()-1).getDate();
+
+        allIndexDatas = filterByDateRange(allIndexDatas,strStartDate, strEndDate);
+
         Map<String,Object> result = new HashMap<>();
         result.put("indexDatas", allIndexDatas);
+        result.put("indexStartDate", indexStartDate);
+        result.put("indexEndDate", indexEndDate);
+
+        return result;
+    }
+
+    private List<IndexData> filterByDateRange(List<IndexData> allIndexDatas, String strStartDate, String strEndDate) {
+        if(StrUtil.isBlankOrUndefined(strStartDate) || StrUtil.isBlankOrUndefined(strEndDate) )
+            return allIndexDatas;
+
+        List<IndexData> result = new ArrayList<>();
+        Date startDate = DateUtil.parse(strStartDate);
+        Date endDate = DateUtil.parse(strEndDate);
+
+        for (IndexData indexData : allIndexDatas) {
+            Date date =DateUtil.parse(indexData.getDate());
+            if(
+                    date.getTime()>=startDate.getTime() &&
+                            date.getTime()<=endDate.getTime()
+            ) {
+                result.add(indexData);
+            }
+        }
         return result;
     }
 }
